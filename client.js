56013cea43f4e5662eccf75284083c853baf89ea
Romeo <romeocorpuz808@gmail.com>
2018-08-08 20:12:00 -1000

    initial commit

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const client = net.createConnection(6969, "localhost", () => {
+const client = net.createConnection(6969, "52.35.253.174", () => {
     console.log("Welcome! You are now chatting!")
     client.on("date", data => {
         console.log(data.toString());
